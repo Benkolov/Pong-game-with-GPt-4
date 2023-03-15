3709270ceeced8710df5f817f787f00aff11a2e2
Boyan <bbenkolov@gmail.com>
2023-03-15 12:49:05 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,71 +1,52 @@
 import tkinter as tk
-import random
 
-# Create the main window
-window = tk.Tk()
-window.title("Pong Game")
+class Pong(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Pong")
+        self.canvas = tk.Canvas(self, width=800, height=400, bg="black")
+        self.canvas.pack()
+        self.bind("<KeyPress>", self.key_press)
+        self.paddle_speed = 20
+        self.ball_speed = 2
+        self.create_objects()
+        self.mainloop()
 
-# Set the size of the window
-window.geometry("600x400")
+    def create_objects(self):
+        self.left_paddle = self.canvas.create_rectangle(30, 150, 40, 250, fill="white")
+        self.right_paddle = self.canvas.create_rectangle(770, 150, 780, 250, fill="white")
+        self.ball = self.canvas.create_oval(390, 190, 410, 210, fill="white")
+        self.dx = self.ball_speed
+        self.dy = self.ball_speed
+        self.animate()
 
-# Create the canvas
-canvas = tk.Canvas(window, bg="black", width=600, height=400)
-canvas.pack()
+    def key_press(self, event):
+        if event.keysym == "Up":
+            self.canvas.move(self.right_paddle, 0, -self.paddle_speed)
+        elif event.keysym == "Down":
+            self.canvas.move(self.right_paddle, 0, self.paddle_speed)
+        elif event.keysym == "w":
+            self.canvas.move(self.left_paddle, 0, -self.paddle_speed)
+        elif event.keysym == "s":
+            self.canvas.move(self.left_paddle, 0, self.paddle_speed)
 
-# Create the ball
-ball = canvas.create_oval(290, 190, 310, 210, fill="white")
+    def animate(self):
+        self.canvas.move(self.ball, self.dx, self.dy)
+        x1, y1, x2, y2 = self.canvas.coords(self.ball)
+        lp_x1, lp_y1, lp_x2, lp_y2 = self.canvas.coords(self.left_paddle)
+        rp_x1, rp_y1, rp_x2, rp_y2 = self.canvas.coords(self.right_paddle)
 
-# Create the paddles
-left_paddle = canvas.create_rectangle(10, 150, 30, 250, fill="white")
-right_paddle = canvas.create_rectangle(570, 150, 590, 250, fill="white")
+        if y1 <= 0 or y2 >= 400:
+            self.dy = -self.dy
 
-# Set the initial speed of the ball
-speed_x = random.randint(2, 5)
-speed_y = random.randint(2, 5)
+        if (x1 <= lp_x2 and y1 >= lp_y1 and y2 <= lp_y2) or (x2 >= rp_x1 and y1 >= rp_y1 and y2 <= rp_y2):
+            self.dx = -self.dx
 
-# Define the function to move the ball
-def move_ball():
-    global speed_x, speed_y
-    canvas.move(ball, speed_x, speed_y)
-    ball_pos = canvas.coords(ball)
-    if ball_pos[1] <= 0 or ball_pos[3] >= 400:
-        speed_y = -speed_y
-    if ball_pos[0] <= 0:
-        canvas.create_text(300, 200, text="Game Over - Left Wins!", font=("Arial", 20), fill="white")
-        window.after(3000, window.destroy)
-    elif ball_pos[2] >= 600:
-        canvas.create_text(300, 200, text="Game Over - Right Wins!", font=("Arial", 20), fill="white")
-        window.after(3000, window.destroy)
-    elif canvas.coords(left_paddle)[2] >= ball_pos[0] >= canvas.coords(left_paddle)[0] and canvas.coords(left_paddle)[3] >= ball_pos[1] >= canvas.coords(left_paddle)[1]:
-        speed_x = -speed_x
-    elif canvas.coords(right_paddle)[0] <= ball_pos[2] <= canvas.coords(right_paddle)[2] and canvas.coords(right_paddle)[3] >= ball_pos[1] >= canvas.coords(right_paddle)[1]:
-        speed_x = -speed_x
-    window.after(20, move_ball)
+        if x1 < 0 or x2 > 800:
+            self.canvas.move(self.ball, 390 - x1, 190 - y1)
+            self.dx = -self.dx
 
-# Define the function to move the left paddle
-def move_left_paddle(event):
-    if event.keysym == "Up":
-        canvas.move(left_paddle, 0, -20)
-    elif event.keysym == "Down":
-        canvas.move(left_paddle, 0, 20)
+        self.after(10, self.animate)
 
-# Define the function to move the right paddle
-def move_right_paddle(event):
-    if event.keysym == "w":
-        canvas.move(right_paddle, 0, -20)
-    elif event.keysym == "s":
-        canvas.move(right_paddle, 0, 20)
-
-# Bind the arrow keys to move the left paddle
-canvas.bind_all("<Up>", move_left_paddle)
-canvas.bind_all("<Down>", move_left_paddle)
-
-# Bind the "w" and "s" keys to move the right paddle
-canvas.bind_all("<w>", move_right_paddle)
-canvas.bind_all("<s>", move_right_paddle)
-
-# Move the ball
-move_ball()
-
-# Start the main loop
-window.mainloop()
+if __name__ == "__main__":
+    Pong()
